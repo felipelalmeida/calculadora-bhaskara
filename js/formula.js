59fcba04d5310bf264c9c81e74d89c9ce509b54b
Felipe Almeida <almeida.felipe98@gmail.com>
2019-09-03 13:11:39 -0300

    update
    recebendo somente numeros no formulario, não receb formulario vazio, mostrando resultado fracionado, não recebe A = 0

--- js/formula.js
+++ js/formula.js
@@ -1,32 +1,23 @@
+let btnCalcular = document.querySelector("#botao-calcular");
 
-var btnCalcular = document.querySelector("#botao-calcular");
-var btnLimpar = document.querySelector("#botao-limpar");
-var resposta1 = document.querySelector("#x-1")
-var resposta2 = document.querySelector("#x-2")
 
 btnCalcular.addEventListener("click", function (event) {
-    event.preventDefault();     // previne o comportamento padrao do btn de enviar os dados e recarregar a pagina
-    var form = document.querySelector("#formulario");
-    var dados = dadosFormulario(form);
-    var resultado = delta(dados);
-    exibeResultado(dados, resultado);
+    event.preventDefault();
+    let form = document.querySelector("#formulario");
+    let dados = dadosFormulario(form);
 
-    resposta1.classList.remove("resposta");
-    resposta2.classList.remove("resposta");
+    if (dados.a == "" || dados.b == "" || dados.c == "") {
+        alert("Preencha todos os campos!");
+    } else if (dados.a == 0) {
+        alert("O valor de A não pode ser 0!");
+    } else {
+        let calculoDelta = delta(dados);
+        exibeResultado(dados, calculoDelta);
+    }
 });
-
-
-btnLimpar.addEventListener("click", function () {
-    var form = document.querySelector("#formulario");
-    form.reset();
-    
-    resposta1.classList.add("resposta");
-    resposta2.classList.add("resposta");
-});
-
 
 function dadosFormulario(form) {
-    var dados = {
+    let dados = {
         a: form.a.value,
         b: form.b.value,
         c: form.c.value
@@ -35,26 +26,49 @@ function dadosFormulario(form) {
 }
 
 function delta(dados) {
-    var b2 = Math.pow(dados.b, 2);
+    let b2 = Math.pow(dados.b, 2);
     console.log("b2 " + b2);
 
-    var ac = 4 * dados.a * dados.c;
+    let ac = 4 * dados.a * dados.c;
     console.log("ac " + ac);
 
-    var resultado = Math.sqrt(b2 - ac);
+    let calculoDelta = (b2 - ac);
+    console.log("Delta " + calculoDelta);
 
-    console.log("resultado " + resultado);
-
-    return resultado;
+    return calculoDelta;
 }
 
-function exibeResultado(dados, resultado) {
-    var x1 = ((-dados.b + resultado)) / (2 * dados.a);
-    var xLinha = document.querySelector("#x-1");
-    xLinha.innerHTML = x1;
+function exibeResultado(dados, calculoDelta) {
 
-    var x2 = ((-dados.b - resultado)) / (2 * dados.a);
-    var xDLinha = document.querySelector("#x-2");
-    xDLinha.innerHTML = x2;
+    if (calculoDelta < 0) {
+        console.log("Valor de Delta negativo");
+        resposta1.classList.add("resposta");
+        resposta2.classList.add("resposta");
+        alert("Valor de Delta negativo, não existem raízes reais.");
+    } else {
+        let resultado = Math.sqrt(calculoDelta);
+        let divisor = 2 * dados.a;
+        let x1;
+        if ((-dados.b + resultado) % divisor == 0){
+            x1 = (-dados.b + resultado) / divisor;
+        } else {
+            x1 = (-dados.b + resultado) + "/" + divisor;
+        };
+        console.log("X'= " + x1);
+        let xLinha = document.querySelector("#x-1");
+        xLinha.innerHTML = x1;
 
+        let x2;
+        if ((-dados.b - resultado) % divisor == 0){
+            x2 = (-dados.b - resultado) / divisor;
+        } else {
+            x2 = (-dados.b - resultado) + "/" + divisor;
+        };
+        console.log("X''= " + x2);
+        let xDLinha = document.querySelector("#x-2");
+        xDLinha.innerHTML = x2;
+
+        resposta1.classList.remove("resposta");
+        resposta2.classList.remove("resposta");
+    }
 }
